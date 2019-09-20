b8fb45602e4b9845a126b9476bce41a3c9919c17
DedeSnrwn13 <sunarwandede13@gmail.com>
2019-09-20 07:44:07 +0700

    push-3

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,27 @@ const hamburger = document.getElementById("sidebar");
 hamburger.addEventListener('click',()=>{
 	$(".navbar-responsive").toggleClass("actived");
 });
+
+// const img_trigger = document.querySelectorAll('.img-hoax');
+// img_trigger.addEventListener('click',()=>{
+// 	let data = this.attr(src);
+// 	console.log(data);
+// 	$('.img-popup').addClass('active');
+// })();\
+
+
+$('.popup-img-trigger').click(function(){
+	const img_source = $(this).children('img');
+	let src_data = img_source.attr('src');
+	console.log(src_data);
+	$('.popup-backdrop').addClass('active');
+	$('.popup-img').attr('src',src_data);
+});
+
+$('.popup-img').click(function(){
+	event.stopPropagation();
+})
+
+$('.popup-backdrop').click(function(){
+	$(this).removeClass('active');
+});
